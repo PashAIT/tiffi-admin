879aefbecc53258b7472205e812d3d039e8b53c2
Pash <pashkarapetyan.ait@gmail.ru>
2022-05-24 13:12:01 +0400

    tiffi-admin start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+import Header from "./components/header/header.vue";
+
+export default defineComponent({
+  components: { Header },
+  name: "App",
+});
+</script>
+<style lang="scss"></style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="row items-center q-pt-xl q-pl-xl">
+    <div class="logo col-2">
+      <img src="images/tiffi-logo.png" class="" alt="tiffi-logo" />
+    </div>
+    <nav class="col-4">
+      <ul class="row items-center">
+        <li>Категория</li>
+        <li>Продукты</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+header {
+  font-family: monserrat;
+  nav {
+    ul {
+      li {
+        font-style: normal;
+        font-weight: 400;
+        font-size: 30px;
+        line-height: 36px;
+      }
+    }
+  }
+}
+</style>
