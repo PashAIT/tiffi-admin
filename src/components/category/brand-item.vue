4b0c93bb19f5bbbee78fd5fcd2b36c1f501f84ea
Pash <pashkarapetyan.ait@gmail.ru>
2022-06-15 23:35:02 +0400

    brands edit and delete modals

---
+++ src/components/category/brand-item.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="brand col-6 relative-position"
+    @mouseover="showTools = true"
+    @mouseleave="showTools = false"
+  >
+    <p class="cursor-pointer">{{ item.title }}</p>
+    <div class="tools absolute-top-right row" v-show="showTools">
+      <edit-categories-tools-wrapper
+        :bgColor="`grey`"
+        :size="`0.7vw`"
+        class="col-6"
+      />
+      <delete-tools-wrapper :bgColor="`grey`" :size="`0.7vw`" class="col-6" />
+    </div>
+  </div>
+</template>
+
+<script>
+import DeleteToolsWrapper from "../tools-wrapper/delete-tools-wrapper.vue";
+import editCategoriesToolsWrapper from "../tools-wrapper/edit-categories-tools-wrapper.vue";
+export default {
+  components: { editCategoriesToolsWrapper, DeleteToolsWrapper },
+  props: ["item"],
+  data() {
+    return {
+      showTools: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.brand {
+  font-family: monserrat;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.7vw;
+  line-height: 2.29166666667vw;
+  .tools {
+    top: -55%;
+    right: 5%;
+  }
+}
+</style>
--- src/components/category/brand-list.vue
+++ src/components/category/brand-list.vue
@@ -1,24 +1,13 @@
 <template>
   <div class="brands row justify-between">
-    <div
-      class="brand col-6 relative-position"
-      v-for="item in brands"
-      :key="item.id"
-    >
-      <p class="cursor-pointer">{{ item.title }}</p>
-      <!-- <tools-wrapper
-        :bgColor="`grey`"
-        :size="`0.7vw`"
-        class="absolute-right tools"
-      /> -->
-    </div>
+    <brand-item v-for="item in brands" :key="`item${item.id}`" :item="item" />
   </div>
 </template>
 
 <script>
-// import toolsWrapper from "../tools-wrapper/delete-tools-wrapper.vue";
+import brandItem from "./brand-item.vue";
 export default {
-  // components: { toolsWrapper },
+  components: { brandItem },
   props: ["brands"],
 };
 </script>
@@ -26,22 +15,5 @@ export default {
 <style lang="scss" scoped>
 .brands {
   height: 39.4444444444vh;
-  .brand {
-    font-family: monserrat;
-    font-style: normal;
-    font-weight: 400;
-    font-size: 1.7vw;
-    line-height: 2.29166666667vw;
-    .tools {
-      top: -55%;
-      right: 5%;
-      display: none;
-    }
-  }
-  .brand:hover {
-    .tools {
-      display: block;
-    }
-  }
 }
 </style>
